963c1ee299a98a1890c6bc98a2a24ab9ab2dce6a
pavankumar <pavankumar@Compassitess-MacBook-Pro.local>
2017-11-01 01:03:07 +0530

    my first commit

---
+++ src/test/java/gitsubmit1/gitdemo1.java
@@ -0,0 +1,12 @@
+package gitsubmit1;
+
+/**
+ * Created by pavankumar on 10/31/17.
+ */
+public class gitdemo1 {
+    public static void main(String[]args)
+    {
+        System.out.println("Trying to commit the first 1");
+        System.out.println("Trying to commit the first 2");
+    }
+}
